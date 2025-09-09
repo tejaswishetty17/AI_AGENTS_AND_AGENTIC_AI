9901ac6bb346edb9d84b83ecd01170f6640d5e72
tejaswishetty17 <shettytejaswi17@gmail.com>
2025-09-09 16:49:32 +0530

    streaming and langchain deployment

--- 3-Debugging/openai_agent.py
+++ 3-Debugging/openai_agent.py
@@ -36,7 +36,40 @@ def make_default_graph():
     return agent
 
 
-agent = make_default_graph()
+def make_alternative_graph():
+    """Make a tool-calling agent"""
+
+    @tool
+    def add(a:float, b:float):
+        """Add two numbers."""
+        return a+b
+    
+    tool_node = ToolNode([add])
+    model_with_tool = model.bind_tools([add])
+    
+    def call_model(state):
+        return {"messages":[model_with_tool.invoke(state["messages"])]}
+    
+    def should_continue(state:State):
+        if state["messages"][-1].tool_calls:
+            return "tools"
+        else: 
+            return END
+        
+    graph_workflow = StateGraph(State)
+
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_node("tools", tool_node)
+
+    graph_workflow.add_edge("tools", "agent")
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_conditional_edges("agent", should_continue)
+
+    agent = graph_workflow.compile()
+    return agent
+
+
+agent = make_alternative_graph()
 
 
 
